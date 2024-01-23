5a79b49ea9acc522485607d2c1fe2a10ab6b2bb2
Nicholas Chai <nicholas.chai@outlook.com>
2024-01-23 20:27:24 +0800

    setup database models

--- index.js
+++ index.js
@@ -1,12 +1,18 @@
+require("dotenv").config();
 const express = require("express");
+console.log(process.env.DB_NAME);
+
+const db = require("./db");
+const models = require("./models");
+models.init();
+
 const app = express();
 
 app.get("/", (req,res) => {
     res.send("hello world");
 });
 
-app.listen(3000, () => {
-    console.log("listening on 3000");
+const port = process.env.PORT || 3000;
+app.listen(port, () => {
+    console.log(`listening on ${port}`);
 });
-
-
